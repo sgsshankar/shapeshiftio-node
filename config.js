aa81444ab34474939fc1807cfab46fdfbdcd7f20
sgsshankar <sgsshankarnarayanan@gmail.com>
2015-08-31 00:49:48 +0530

    Fixed configuration issue in npm

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ exports.config = {
 	transaction: '/shift',
 	receipt: '/mail',
 	fixedTransaction: '/sendamount',
-	cancelTransaction: '/cancelpending'
+	cancelTransaction: '/cancelpending',
 	apiKey: 'd024c31b3424379c611f06a2dceb934ea624a82dd587831157cb6ddc64e144db8fa3091a607a4d02489660d795257894fdc840f89767b61938a1c9033ee28e6a'
 };
